961193a0798218b02dff757dffc3597a11dfb009
German Gambarte <germangambarte@gmail.com>
2024-12-21 14:27:48 -0300

    update

---
+++ nvim.advent/init.lua
@@ -0,0 +1,20 @@
+print("advent of neovim")
+
+require("config.lazy")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+
+
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+vim.keymap.set('n', '<space>x', ':.lua<CR>')
+vim.keymap.set('v', '<space>x', ':lua<CR>')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-higlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
