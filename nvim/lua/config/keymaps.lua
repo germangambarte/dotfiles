ef164b58c87c9072d4768b2efb47a4620916d0c3
German Gambarte <germangambarte@gmail.com>
2025-04-03 16:04:33 -0300

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,82 +1,27 @@
-local set = vim.keymap.set
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
 
--- swap selected lines
-set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Swap whit line below' })
-set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Swap whit line above' })
-
-set('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
-set('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
-
--- keymap('n', '<leader>a', 'ggVG', { desc = 'Select all' })
-set('n', '<C-d>', '<C-d>zz')
-set('n', '<C-u>', '<C-u>zz')
-set('n', 'n', 'nzzzv')
-set('n', 'N', 'Nzzzv')
-
--- buffers
--- set('n', '<S-h>', '<cmd>bprevious<CR>')
--- set('n', '<S-l>', '<cmd>bnext<CR>')
-set('n', '<leader>bd', '<cmd>:bdelete<CR>')
-
--- tabs
--- set('n', '<left>', 'gT')
--- set('n', '<right>', 'gt')
-
--- greatest remap ever
-set('n', '<leader>p', [["+]])
-
--- keep things highlighted after moving with < and >
-set('v', '<', '<gv')
-set('v', '>', '>gv')
-
--- next greatest remap ever : asbjornHaland
-set({ 'n', 'v' }, '<leader>y', [["+y]])
-set({ 'n', 'v' }, '<leader>p', [["+p]])
-set({ 'n', 'v' }, '<leader>d', [["+d]])
-
-set('n', 'Q', '<nop>')
-set('n', '<C-s>', '<cmd>w<CR>')
-set('n', '<C-S>', '<cmd>wa<CR>')
-
--- These mappings control the size of splits (height/width)
-set('n', '<M-h>', '<c-w>5<')
-set('n', '<M-l>', '<c-w>5>')
-set('n', '<M-k>', '<C-W>+')
-set('n', '<M-j>', '<C-W>-')
-set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-set('n', '<leader>ch', function()
-  vim.lsp.inlay_hint(0, nil)
-end, { desc = 'Toggle Inlay Hints' })
-
-function CompileAndRun()
-  vim.cmd('w')
-  vim.cmd('!g++ % -o %<')
-  if vim.v.shell_error == 0 then
-    vim.cmd('! %<')
-  end
-end
-
-vim.api.nvim_set_keymap(
-  'n',
-  '<leader>cr',
-  ':lua CompileAndRun()<CR>',
-  { noremap = true, silent = true }
-)
-set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-set(
-  'n',
-  '<leader>q',
-  vim.diagnostic.setloclist,
-  { desc = 'Open diagnostic [Q]uickfix list' }
-)
-set(
-'n',
-'<leader>e',
-':lua MiniFiles.open()<cr>'
-)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+
+vim.keymap.set('n', '<leader>sf', function()require('fzf-lua').files()end, { desc = '[S]earch [F]iles' })
+-- vim.keymap.set('n', '<leader>sf', '<cmd>FzfLua files <cr>', { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', '<cmd>FzfLua helptags <cr>', { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', '<cmd>FzfLua keymaps <cr>', { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sw', '<cmd>FzfLua grep_cword <cr>', { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', '<cmd>FzfLua live_grep <cr>', { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', '<cmd>FzfLua diagnostics_document <cr>', { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', '<cmd>FzfLua files resume=true<cr>', { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>s.', '<cmd>FzfLua oldfiles <cr>', { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader><leader>','<cmd>FzfLua buffers <cr>' , { desc = '[ ] Find existing buffers' })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,65 +1,89 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
 
-vim.opt.background = 'dark'
+-- Convert tabs to spaces
+vim.opt.expandtab = true
 
--- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = true
+-- Base indent width
+vim.opt.shiftwidth = 4
 
-vim.opt.cursorline = true
-vim.opt.fillchars:append({ eob = ' ' })
+-- Spaces per tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
-vim.opt.nu = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
--- sane dafaults
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.wrap = false
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.cindent = true
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
 
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Allow plugins like undotree to work well
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
-vim.opt.undofile = true
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- vim.opt.hlsearch = true
-vim.opt.incsearch = true
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
-vim.opt.sidescrolloff = 10
-vim.opt.signcolumn = 'yes'
-vim.opt.isfname:append('@-@')
 
-vim.opt.updatetime = 50
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
 
-vim.opt.termguicolors = true
-
--- Nice code folding
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.opt.foldcolumn = '0'
-vim.opt.foldtext = ''
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 99
-vim.opt.foldnestmax = 4
-
-vim.opt.colorcolumn = '80'
-vim.opt.guicursor = table.concat({
-  'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100',
-}, ',')
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
